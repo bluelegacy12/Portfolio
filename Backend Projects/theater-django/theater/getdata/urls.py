a603c402b3583bffb8e373349862f7910ffada4b
bluelegacy12 <67515336+bluelegacy12@users.noreply.github.com>
2023-01-14 15:24:44 -0600

    added favorites functionality and background

--- Backend Projects/theater-django/theater/getdata/urls.py
+++ Backend Projects/theater-django/theater/getdata/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('home/', views.home),
     # /info/23/
     re_path(r'^info/(?P<performer_id>[0-9]+)/$', views.info, name='info'),
-    re_path(r'^info/(?P<performer_id>[0-9]+)/favorite/$', views.favorite, name='favorite')
+    re_path(r'^info/(?P<show_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    path('register', views.UserFormView.as_view(), name='register'),
 ]
--- Backend Projects/theater-django/theater/getdata/views.py
+++ Backend Projects/theater-django/theater/getdata/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from .forms import NameForm
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import NameForm, UserForm
 from .models import Performers, Shows, Roles
-
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
 
 all_performers = Performers.objects.all()
 all_shows = Shows.objects.all()
@@ -27,7 +28,7 @@ def addCast(request):
 def home(request):
     if request.method == 'POST':
         form = NameForm(request.POST)
-        name = form.name
+        name = form.get_initial_for_field(name, "Name: name")
         email = form.email
         phone = form.phone
         context = {
@@ -36,6 +37,7 @@ def home(request):
             'email': email,
             'phone': phone
         }
+        performer = Performers.save(name=name, email=email, phone=phone)
         if form.is_valid():
             return render(request, 'added.html', context)
 
@@ -48,3 +50,53 @@ def home(request):
             'all_shows': all_shows,
         }   
     return render(request, 'home.html', context)
+
+def favorite(request, show_id):
+    show = get_object_or_404(Shows, pk=show_id)
+    try:
+        selected_show = Shows.objects.get(pk=request.POST['show'])
+    except (KeyError, Shows.DoesNotExist):
+        return render(request, 'home.html', {
+            'show': show,
+            'error_message': "You did not select a valid show"
+        })
+    else:
+        selected_show.favorite = True
+        selected_show.save()
+        form = NameForm()
+        context = {
+            'all_performers': all_performers,
+            'form': form,
+            'all_roles': all_roles,
+            'all_shows': all_shows,
+        }   
+    return render(request, 'home.html', context)
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'getapp/register.html'
+
+    #display blank form
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    #register user based on input data
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            # unify the data into commit-ready format
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('getdata:home')
+        return render(request, self.template_name, {'form': form})

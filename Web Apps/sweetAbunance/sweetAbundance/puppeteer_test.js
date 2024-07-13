041b96774dd2f53c71ed9217ca46b76be106d2c4
bluelegacy12 <67515336+bluelegacy12@users.noreply.github.com>
2024-07-13 15:34:39 -0500

    updated test action

---
+++ Web Apps/sweetAbunance/sweetAbundance/puppeteer_test.js
@@ -0,0 +1,28 @@
+import puppeteer from "puppeteer";
+
+const url = 'http://127.0.0.1:8000/';
+
+(async()=>{
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+
+    await page.goto(url);
+    const title = await page.evaluate(()=>{
+        const divs = document.querySelectorAll('div');
+        for (let div of divs) {
+            if (div.innerText == 'Sweet Abundance'){
+                return div.innerText;
+            }
+        }
+        return null
+    });
+
+    await browser.close();
+
+    if (title != null){
+        return 0;
+    } else {
+        return 1;
+    }
+    
+})();

68227938530d20bcd62e10ba37735ee461c08cd8
bluelegacy12 <67515336+bluelegacy12@users.noreply.github.com>
2024-07-13 20:07:46 -0500

    updated tests

---
+++ puppeteer_test.js
@@ -0,0 +1,43 @@
+import puppeteer from "puppeteer";
+import * as child from 'child_process';
+
+const url = 'http://127.0.0.1:8000/';
+
+// run the `ls` command using exec
+child.exec('python manage.py runserver ./', (err, output) => {
+    // once the command has completed, the callback function is called
+    if (err) {
+        // log and return if we encounter an error
+        console.error("could not execute command: ", err);
+        return;
+    }
+    // log the output received from the command
+    console.log("Starting server" );
+});
+
+(async()=>{
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+
+    await page.goto(url);
+    const title = await page.evaluate(()=>{
+        const divs = document.querySelectorAll('div');
+        for (let div of divs) {
+            if (div.innerText == 'Sweet Abundance'){
+                return div.innerText;
+            }
+        }
+        return null
+    });
+
+    await browser.close();
+
+    if (title != null){
+        console.log('Success!');
+        return 0;
+    } else {
+        console.log('Failure');
+        return 1;
+    }
+    
+})();
